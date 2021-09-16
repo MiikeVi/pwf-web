e56bbb0de61522dd208810575e0fb9b0d8e035d1
AdrianOlivares77 <adrian.olivares.f.52@gmail.com>
2021-09-16 18:39:05 -0300

    feat(shared): Formsmodule and modals components added

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { TabsComponent } from './components/tabs/tabs.component';
+import { ModalCreatePetComponent } from './components/modal-create-pet/modal-create-pet.component';
+import { ModalEditPetComponent } from './components/modal-edit-pet/modal-edit-pet.component';
 
 const components = [
-  TabsComponent
+  TabsComponent,
+  ModalCreatePetComponent,
+  ModalEditPetComponent
 ];
 
 @NgModule({
   declarations: [... components],
   imports: [
     CommonModule,
-    IonicModule
+    IonicModule,
+    FormsModule
   ],
   exports: [...components]
 })
