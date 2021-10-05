5fe3c723e91b40b26aa78fb63687400f9ba64f2a
AdrianOlivares77 <adrian.olivares.f.52@gmail.com>
2021-10-05 17:31:29 -0300

    feat(login): paths added and redirectionated

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
+import { HomePage } from '../home/home.page';
 import { LoginPage } from './login.page';
 
 const routes: Routes = [
   {
     path: '',
     component: LoginPage
-  }
+  },
+  {
+    path: 'buscar-cuidadores',
+    loadChildren: () => import('../home/screens/caretakers-screen/caretakers-screen.module').then( m => m.CaretakersScreenPageModule)
+  },
+  {
+    path: 'home',
+    component: HomePage
+  },
 ];
 
 @NgModule({
